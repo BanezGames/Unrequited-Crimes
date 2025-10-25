1fb11c2354c8a664960ae68241c6d066f99a591a
BanezGames <KCHinton@student.fullsail.edu>
2025-10-24 22:47:12 -0500

    shoot create

--- UnRequited Crimes/Assets/Scripts/playerController.cs
+++ UnRequited Crimes/Assets/Scripts/playerController.cs
@@ -10,10 +10,16 @@ public class playerController : MonoBehaviour
     [SerializeField] int jumpCountMax;
     [SerializeField] int gravity;
 
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootdist;
+    [SerializeField] float shootRate;
+
+
     Vector3 moveDir;
     Vector3 playerVel;
 
     int jumpCount;
+    float shootTimer;
 
     bool isSprinting;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,6 +31,7 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootdist, Color.red);
         movement();
 
         sprint();
@@ -47,6 +54,11 @@ public class playerController : MonoBehaviour
         jump();
         controller.Move(playerVel * Time.deltaTime);
 
+        if(Input.GetButton("Fire1") && shootTimer >= shootRate)
+        {
+            shoot();
+        }
+
     }
     void sprint()
     {
@@ -67,5 +79,18 @@ public class playerController : MonoBehaviour
             playerVel.y = jumpSpeed;
             jumpCount++;
         }
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootdist))
+        {
+            Debug.Log(hit.collider.name);
+        
+        }
+
     }
 }
